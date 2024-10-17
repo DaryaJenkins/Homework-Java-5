e584ad5eff57cbce4ecd23b073d8b99e4636f935
Darya Jenkins <melissegreenleaf@gmail.com>
2024-10-17 23:16:34 +0200

    csv added

--- src/test/java/CalculationServiceTest.java
+++ src/test/java/CalculationServiceTest.java
@@ -1,21 +1,17 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.CalculationService;
 
 public class CalculationServiceTest {
 
-    @Test
-    public void vacationSalary10K() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/money.csv")
+    public void vacationSalary(int income, int expenses, int threshold) {
         CalculationService service = new CalculationService();
 
-        int vacationMonths = service.calculate(10_000, 3_000, 20_000);
-        System.out.println(vacationMonths);
-    }
-
-    @Test
-    public void vacationSalary100K() {
-        CalculationService service = new CalculationService();
-
-        int vacationMonths = service.calculate(100_000, 60_000, 150_000);
+        int vacationMonths = service.calculate(income, expenses, threshold);
         System.out.println(vacationMonths);
     }
 }
